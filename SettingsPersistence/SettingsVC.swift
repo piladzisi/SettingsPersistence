00c1c5b52ca9b92937af2b52a47fc7184bce40da
piladzisi <piladzisi@gmail.com>
2021-01-10 01:08:31 +0200

    save settings to userDefaults

--- SettingsPersistence/SettingsVC.swift
+++ SettingsPersistence/SettingsVC.swift
@@ -13,18 +13,51 @@ class SettingsVC: UITableViewController {
     @IBOutlet weak var lastNameTextField: UITextField!
     @IBOutlet weak var likesDevelopmentToggle: UISwitch!
     @IBOutlet weak var coolMeterValueLabel: UILabel!
-    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var coolMeterSlider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadSettings()
     }
     
     @IBAction func didSlideCoolSlider(_ sender: UISlider) {
         coolMeterValueLabel.text = String(format: "%.0f", sender.value)
     }
     
+    func loadSettings() {
+        guard
+            let settingsDictionary = UserDefaults.standard.dictionary(forKey: "settings"),
+            let firstName = settingsDictionary["firstName"] as? String,
+            let lastName = settingsDictionary["lastName"] as? String,
+            let likesDevelopment = settingsDictionary["likesDevelopment"] as? Bool,
+            let coolMeterValue = settingsDictionary["coolMeterValue"] as? Int
+            else { return }
+        
+        firstNameTextField.text = firstName
+        lastNameTextField.text = lastName
+        likesDevelopmentToggle.isOn = likesDevelopment
+        coolMeterValueLabel.text = String(coolMeterValue)
+        coolMeterSlider.value = Float(coolMeterValue)
+    }
+    
+    func saveSettings() {
+        let settingsDictionary: [String: Any?] = ["firstName": firstNameTextField.text,
+                                                  "lastName": lastNameTextField.text,
+                                                  "likesDevelopment": likesDevelopmentToggle.isOn,
+                                                  "coolMeterValue": Int(coolMeterSlider.value)]
+        UserDefaults.standard.set(settingsDictionary, forKey: "settings")
+    }
+    
+    func didSaveAlert() {
+        let alert = UIAlertController(title: "Saved Settings", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Dismiss", style: .cancel)
+        alert.addAction(action)
+        present(alert, animated: true)
+    }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        guard indexPath.section == 2 else { return }
-        print("save")
+        guard indexPath.section == 2, indexPath.row == 0 else { return }
+        saveSettings()
+        didSaveAlert()
     }
 }
